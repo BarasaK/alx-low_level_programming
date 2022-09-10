bdbbcf9bb151c4f2c21c68d730b9b1ae535946ca
barasaK <nicholas.k.barasa@gmail.com>
2022-09-09 19:48:04 -0700

    func

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Start here
+ * Description: prints some statetment
+ * Return: 0
+ * /
+ 
+ int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
