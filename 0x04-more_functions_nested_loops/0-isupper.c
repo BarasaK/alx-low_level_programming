18753dbd4ab82fefb156fe53d9a48bdbfd8a674a
barasaK <nicholas.k.barasa@gmail.com>
2022-09-18 11:11:32 -0700

    upper:

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry
+ * 
+ * @c: c is a variable name
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isupper(int c)
+{
+if (c >= 97 && c <= 122)
+return (0);
+if (c >= 65 && c <= 90)
+return (1);
+
+return (0);
+}
+
---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,11 @@
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_number(0);
+	_putchar('\n');
+	return (0);
+}
